b2d17d24a6e8c4df90436c26f6d4251b6699cebd
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-28 22:46:16 +0530

    Create 03_username.py

---
+++ 03_username.py
@@ -0,0 +1,12 @@
+first_name = input().strip()
+last_name = input().strip()
+user=first_name[0]+last_name[0]
+first_name=first_name.lower()
+last_name=last_name.lower()
+print(f"Username: {first_name}{last_name}")
+print(f"Initials: {user}")
+
+
+# output
+# Username: kaitove
+# Initials: KT
