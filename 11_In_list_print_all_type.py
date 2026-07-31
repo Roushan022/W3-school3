34c4fdf27a0e220c745bba9c44d022fa89f2d855
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-31 21:39:49 +0530

    Print types of various data in a list

---
+++ 11_In_list_print_all_type.py
@@ -0,0 +1,12 @@
+details=[1252,11.23,1+2j,True,'Roushan',(0,1),[5,12],{"class":"V","Section":"A"}]
+for i in details:
+    print(f"Type of {i} is {type(i)}")
+
+# Type of 1252 is <class 'int'>
+# Type of 11.23 is <class 'float'>
+# Type of (1+2j) is <class 'complex'>
+# Type of True is <class 'bool'>
+# Type of Roushan is <class 'str'>
+# Type of (0, 1) is <class 'tuple'>
+# Type of [5, 12] is <class 'list'>
+# Type of {'class': 'V', 'Section': 'A'} is <class 'dict'>
