000a2a21e71423b2972c0bab2a05a72351db0492
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-08-01 12:01:32 +0530

    Comment out password validation logic
    Commented out the password validation code block.

--- 17_pass_validation.py
+++ 17_pass_validation.py
@@ -22,4 +22,26 @@ while(True):
     else:
         print("valid Pass")
         break
+
         
+#without import re         
+# password = input("Enter a password: ")
+# upper = lower = digit = special = False
+# space = False
+
+# for ch in password:
+#     if ch.isupper():
+#         upper = True
+#     elif ch.islower():
+#         lower = True
+#     elif ch.isdigit():
+#         digit = True
+#     elif ch in "@#$":
+#         special = True
+#     elif ch.isspace():
+#         space = True
+
+# if len(password) >= 6 and upper and lower and digit and special and not space:
+#     print("Valid Password")
+# else:
+#     print("Invalid Password")
